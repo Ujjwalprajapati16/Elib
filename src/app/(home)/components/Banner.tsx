b2fc1914e37d905d28ce3bbdf0814371d682635e
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-09-16 19:48:01 +0530

    Reorganised components

--- src/app/(home)/components/Banner.tsx
+++ src/app/(home)/components/Banner.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import PapperBg from '../../public/banner.png'
-import BookImage from '../../public/bookimg.webp'
 
 const Banner = () => {
     return (
         <div className='mx-auto max-w-7xl px-5 py-10'>
             <div className='relative'>
                 <Image
-                    src={PapperBg}
+                    src={'/banner.png'}
                     alt='billboard'
                     className='h-72 w-full rounded-lg'
                     height={0}
@@ -16,7 +14,7 @@ const Banner = () => {
                     sizes='100vw'
                 />
             </div>
-            <div className='absolute insert-0 h-full rounded-lg bg-gray-950 opacity-30'>
+            {/* <div className='absolute insert-0 h-full rounded-lg bg-gray-950 opacity-30'>
                 <Image
                     src={BookImage}
                     alt='billboard'
@@ -29,7 +27,7 @@ const Banner = () => {
                 <h3 className='absolute left-10 top-1/2 w-fill max-w-3xl -translate-y-1/2 text-5xl font-semibold tracking-tight text-white'>
                     E-Lib - Your Gateway to Knowledge
                 </h3>
-            </div>
+            </div> */}
         </div>
     )
 }
