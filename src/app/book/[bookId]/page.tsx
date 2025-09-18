d83e1fc925379735761fa44e7be89d5b4b0e3a62
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-09-18 15:00:47 +0530

    add book detail page

---
+++ src/app/book/[bookId]/page.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import React from 'react'
+
+const page = async ({ params }: { params: { bookId: string } }) => {
+    let book;
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}books/${params.bookId}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch book details');
+        }
+        book = await response.json();
+    } catch (error) {
+        throw new Error('Error fetching book details');
+    }
+
+    if (!book) {
+        throw new Error('Book not found');
+    }
+
+    return (
+        <div className='mx-auto grid max-w-6xl grid-cols-3 gap-10 px-5 py-10'>
+            <div className="col-span-2 pr-16 text-primary-950">
+                <h2 className='mb-5 text-5xl font-bold leading-[1.1]'>{book.title}</h2>
+                <span className='font-semibold'>by {book.author.name}</span>
+                <p className='mt-5 text-lg leading-5'>{book.description}</p>
+            </div>
+            <div className="flex justify-end">
+                <Image
+                    src={book.coverImage}
+                    alt={book.title}
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{ width: 'auto', height: 'auto' }}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default page
